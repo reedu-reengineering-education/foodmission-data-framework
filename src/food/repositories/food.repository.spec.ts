0e103984e8c3162a8276e670fbf64287b92bdb94
Maria Zadnepryanets <mariya.z@gmail.com>
2025-12-03 13:25:15 +0100

    fix: failing tests

--- src/food/repositories/food.repository.spec.ts
+++ src/food/repositories/food.repository.spec.ts
@@ -1,5 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { Prisma } from '@prisma/client';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { FoodRepository, UpdateFoodDto } from './food.repository';
 import { CreateFoodDto } from '../dto/create-food.dto';
