76c42990c8efc27d72d42798338c930ee9229f84
Maria Zadnepryanets <mariya.z@gmail.com>
2025-12-08 22:51:43 +0100

    fix: import missing repositories

--- src/recipe/recipe.module.ts
+++ src/recipe/recipe.module.ts
@@ -5,11 +5,12 @@ import { RecipeRepository } from './repositories/recipe.repository';
 import { DatabaseModule } from '../database/database.module';
 import { CommonModule } from '../common/common.module';
 import { DishModule } from '../dish/dish.module';
+import { UserRepository } from '../user/repositories/user.repository';
 
 @Module({
   imports: [DatabaseModule, CommonModule, DishModule],
   controllers: [RecipeController],
-  providers: [RecipeService, RecipeRepository],
+  providers: [RecipeService, RecipeRepository, UserRepository],
   exports: [RecipeService, RecipeRepository],
 })
 export class RecipeModule {}
