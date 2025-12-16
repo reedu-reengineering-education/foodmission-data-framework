c85776b3bd2cbc901a8e8ef5acb886c455746417
Maria Zadnepryanets <mariya.z@gmail.com>
2025-12-16 12:31:46 +0100

    fix: after refactoring

--- src/dish/dto/update-dish.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMealDto } from './create-dish.dto';
-
-export class UpdateMealDto extends PartialType(CreateMealDto) {}
--- src/dish/dto/update-meal.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMealDto } from './create-meal.dto';
-
-export class UpdateMealDto extends PartialType(CreateMealDto) {}
