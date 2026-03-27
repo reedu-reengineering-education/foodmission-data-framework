8347eb97b518fc2a239a1c9a345df87a1ba38c8d
Felix Erdmann <hello@felixerdmann.com>
2026-03-27 10:14:35 +0100

    fix: add modules to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,18 @@ import { MealRecipeModule } from './app/meal-recipe.module';
 import { ShoppingPantryModule } from './app/shopping-pantry.module';
 import { UserCommunityModule } from './app/user-community.module';
 import { LoggingMiddleware } from './common/middleware/logging.middleware';
+import { LoggingModule } from './common/logging/logging.module';
 import { MonitoringMiddleware } from './monitoring/monitoring.middleware';
+import { MonitoringModule } from './monitoring/monitoring.module';
 import { SecurityMiddleware } from './security/middleware/security.middleware';
+import { SecurityModule } from './security/security.module';
 
 @Module({
   imports: [
     InfrastructureModule,
+    LoggingModule,
+    MonitoringModule,
+    SecurityModule,
     UserCommunityModule,
     CatalogFeatureModule,
     ShoppingPantryModule,
