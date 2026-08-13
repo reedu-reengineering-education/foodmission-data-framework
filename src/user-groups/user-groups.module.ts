47414658bc82afef1bd90563293af729cbfb126f
Maria Zadnepryanets <mariya.z@gmail.com>
2026-08-13 12:47:57 +0200

    feat: Emit USER_GROUP_JOINED and USER_GROUP_LEFT from membership changes
    Record ledger facts when a registered user creates, joins, leaves, or
    is removed from a group. Skip virtual members.

--- src/user-groups/user-groups.module.ts
+++ src/user-groups/user-groups.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { CommonModule } from '../common/common.module';
 import { UsersModule } from '../users/users.module';
+import { EventsModule } from '../events/events.module';
 import { UserGroupController } from './controllers/user-groups.controller';
 import { UserGroupService } from './services/user-groups.service';
 import { UserGroupRepository } from './repositories/user-groups.repository';
 import { GroupMembershipRepository } from './repositories/group-memberships.repository';
 
 @Module({
-  imports: [DatabaseModule, CommonModule, UsersModule],
+  imports: [DatabaseModule, CommonModule, UsersModule, EventsModule],
   controllers: [UserGroupController],
   providers: [UserGroupService, UserGroupRepository, GroupMembershipRepository],
   exports: [UserGroupService, UserGroupRepository],
