680f00a14d9b4512e0626152bcfff1369e1c30ac
Umut <u.tas@reedu.de>
2026-09-21 11:46:56 +0200

    fix: imports

--- src/foody/foody.module.ts
+++ src/foody/foody.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { CommonModule } from '../common/common.module';
 import { EventsModule } from '../events/events.module';
 import { GamificationModule } from '../gamification/gamification.module';
+import { UsersRepository } from '../users/repositories/users.repository';
 import { FoodyController } from './controllers/foody.controller';
 import { FoodyRepository } from './repositories/foody.repository';
 import { FoodyService } from './services/foody.service';
 
 @Module({
-  imports: [DatabaseModule, EventsModule, GamificationModule],
+  imports: [DatabaseModule, CommonModule, EventsModule, GamificationModule],
   controllers: [FoodyController],
-  providers: [FoodyService, FoodyRepository],
+  providers: [FoodyService, FoodyRepository, UsersRepository],
   exports: [FoodyService, FoodyRepository],
 })
 export class FoodyModule {}
